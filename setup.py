c13ca975485c6ccf8d3b783c136be98b15647c18
Cody Kochmann <kochmanncody@gmail.com>
2022-05-14 10:27:56 -0600

    added initial files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from distutils.core import setup
+import sys, os, setuptools
+
+version = '2022.5.14'
+name = 'litecollections'
+packages = setuptools.find_packages()
+
+assert name in packages, [name, packages]  # if package name doesnt show up, something is wrong
+
+
+setup(
+  name = name,
+  version = version,
+  packages = packages,
+  install_requires = [],
+  zip_safe=True,
+  description = 'python collections except its all backed by sqlite',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/litecollections',
+  download_url = 'https://github.com/CodyKochmann/litecollections/tarball/{}'.format(version),
+  keywords = ['sqlite', 'collections', 'persistent', 'performance', 'scale', 'datascience', 'acid', 'database'],
+  classifiers = []
+)
